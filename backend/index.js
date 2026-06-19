b36757bf6c4779e0be8073ed7cc539421cfb6f4d
likhithaj04 <likhitha15jayram@gmail.com>
2026-06-19 11:16:50 +0530

    added components to recieve backend data

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,25 @@ import Groq from "groq-sdk";
 import express from 'express'
 import {tavily }from "@tavily/core";
 import readline from 'node:readline/promises'
-const tvly = tavily({ apiKey: process.env.TAVILY_API_KEY });
+import cors from 'cors'
+
+const app=express();
+app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 
 const groq = new Groq( {apiKey:process.env.GROQ_API_KEY});
-async function main() {
-        const rl=readline.createInterface({input:process.stdin, output:process.stdout})
+const tvly = tavily({ apiKey: process.env.TAVILY_API_KEY });
+
+
+app.post("/search",async(req,res)=>{
+  try{
+    const question=req.body.query;
+    console.log(question);   
+
+    //  const rl=readline.createInterface({input:process.stdin, output:process.stdout})
 
  const message= [
         {
@@ -18,23 +32,21 @@ async function main() {
             current date and time :${new Date().toUTCString()}
             `
         },
-      // {
-      //   role: "user",
-      //   content: "Rupee",
-      // },
     ]
-    while(true){
+  
    
-      const question=await rl.question("You : ")
+      // const question=await rl.question("You : ")
 
-      if(question=== 'stop'){
-        break
-      }
+      // if(question=== 'stop'){
+      //   break;
+      // }
     message.push({
       role:'user',
       content:question
     })
       while(true){
+        console.log("1 Request received");
+
 const completion = await groq.chat.completions.create({
     model: "openai/gpt-oss-120b",
     messages:message,
@@ -59,66 +71,159 @@ const completion = await groq.chat.completions.create({
   ],
   tool_choice:'auto'
   });
-  
+  // console.log("2 Completion received");
+
   message.push(completion.choices[0].message)
   const toolcall= completion.choices[0].message.tool_calls;
 
   if(!toolcall){
     console.log(`Asst : ${completion.choices[0].message.content}`);
-    break;
-  }
+return res.send({
+        status: 200,
+        message: "success",
+        data: completion.choices[0].message.content
+    });  }
   
  for(const tool of toolcall){
     // console.log("tools : ",tool);
     const functionName=tool.function.name;
     const params=tool.function.arguments
 
-   
-
     if(functionName=="searchWeb"){
       let toolresult=await searchWeb(JSON.parse(params))
-      // console.log(toolresult);
+      // console.log("tool res",toolresult);
        message.push({
     role:'tool',
         tool_call_id:tool.id,
     content:toolresult
    })
+    return  res.send({status:200,message:"success",data:toolresult})  
+
     }
+ }
+
+}
+
+  }
+  catch(err){
+    console.log(err);
+    
+  }
+})
+
+// async function main() {
+//         const rl=readline.createInterface({input:process.stdin, output:process.stdout})
+
+//  const message= [
+//         {
+//             role:"system",
+//             content:`You are a Smart assistant who provides one to five words  answer on the given provided question
+//             . The data should be accurate as of present time. If no data found return data not available
+//             1.searchWeb({query}:{query:String}) // Search latest information and realtime data  on the internet .
+//             current date and time :${new Date().toUTCString()}
+//             `
+//         },
+//       // {
+//       //   role: "user",
+//       //   content: "Rupee",
+//       // },
+//     ]
+//     while(true){
+   
+//       const question=await rl.question("You : ")
+
+//       if(question=== 'stop'){
+//         break
+//       }
+//     message.push({
+//       role:'user',
+//       content:question
+//     })
+//       while(true){
+// const completion = await groq.chat.completions.create({
+//     model: "openai/gpt-oss-120b",
+//     messages:message,
+//      "tools": [
+//     {
+//       type: "function",
+//       function: {
+//         name: "searchWeb",
+//         description: "Search the web for of for the answer to given provied query",
+//       parameters: {
+//           type: "object",
+//           properties: {
+//             query:{
+//               type: "string",
+//               description: "Search query to perform search on",
+//             }
+//           },
+//           required: ["query"]
+//         }
+//       }
+//     }
+//   ],
+//   tool_choice:'auto'
+//   });
+  
+//   message.push(completion.choices[0].message)
+//   const toolcall= completion.choices[0].message.tool_calls;
+
+//   if(!toolcall){
+//     console.log(`Asst : ${completion.choices[0].message.content}`);
+//     break;
+//   }
+  
+//  for(const tool of toolcall){
+//     // console.log("tools : ",tool);
+//     const functionName=tool.function.name;
+//     const params=tool.function.arguments
+
+   
+
+//     if(functionName=="searchWeb"){
+//       let toolresult=await searchWeb(JSON.parse(params))
+//       // console.log(toolresult);
+//        message.push({
+//     role:'tool',
+//         tool_call_id:tool.id,
+//     content:toolresult
+//    })
+//     }
    
     
- }
-}
-    }
+//  }
+// }
+//     }
   
-  // const completion2 = await groq.chat.completions.create({
-  //   model: "llama-3.3-70b-versatile",
-  //   messages:messsage,
-  //    "tools": [
-  //   {
-  //     type: "function",
-  //     function: {
-  //       name: "searchWeb",
-  //       description: "Get the currency of given countries with comparing to how much of that is to doller",
-  //     parameters: {
-  //         type: "object",
-  //         properties: {
-  //           query:{
-  //             type: "string",
-  //             description: "Search query to perform search on",
-  //           }
-  //         },
-  //         required: ["query"]
-  //       }
-  //     }
-  //   }
-  // ],
-  // tool_choice:'auto'
-  // });
-  // console.log(JSON.stringify(completion2.choices[0].message,null,2));
+//   // const completion2 = await groq.chat.completions.create({
+//   //   model: "llama-3.3-70b-versatile",
+//   //   messages:messsage,
+//   //    "tools": [
+//   //   {
+//   //     type: "function",
+//   //     function: {
+//   //       name: "searchWeb",
+//   //       description: "Get the currency of given countries with comparing to how much of that is to doller",
+//   //     parameters: {
+//   //         type: "object",
+//   //         properties: {
+//   //           query:{
+//   //             type: "string",
+//   //             description: "Search query to perform search on",
+//   //           }
+//   //         },
+//   //         required: ["query"]
+//   //       }
+//   //     }
+//   //   }
+//   // ],
+//   // tool_choice:'auto'
+//   // });
+//   // console.log(JSON.stringify(completion2.choices[0].message,null,2));
 
-  rl.close();
-}
-main().catch(console.error);
+//   rl.close();
+// }
+// main().catch(console.error);
 
 
 async function searchWeb({query}) {
@@ -127,3 +232,9 @@ async function searchWeb({query}) {
  const finalres=response.results.map((result)=>result.content).join('\n\n')
     return finalres;
 }
+
+
+app.listen(8000,()=>{
+  console.log("server running");
+  
+})
--- frontend/src/Home/Chat.jsx
+++ frontend/src/Home/Chat.jsx
@@ -1,9 +1,104 @@
-import React from 'react'
+import React, { useState } from 'react'
+import api from '../utils/api'
 
 export default function Chat() {
+  
+  const [data,setData]=useState('')
+  const [messages,setMessages]=useState([])
+
+ const handlleCLick = async () => {
+  if (!data.trim()) return;
+
+  const userMessage = data;
+
+  setMessages(prev => [
+    ...prev,
+    {
+      role: "user",
+      text: userMessage
+    }
+  ]);
+
+  setData("");
+
+  try {
+    // console.log("res.sent");
+    
+    const res = await api.post("/search", {
+      query: userMessage
+      
+    });
+// console.log("Tool result:", res.data.data);
+    setMessages(prev => [
+      ...prev,
+      {
+        role: "assistant",
+      text: res.data.data      }
+    ]);
+   
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+    const handleChange = (e) => {
+    setData(e.target.value);
+
+    e.target.style.height = "auto";
+    e.target.style.height = `${e.target.scrollHeight}px`;
+
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-500">
-      <h1>SearchGPT</h1>
+   <div className="h-screen flex flex-col bg-gray-300">
+      
+      <div className="flex-1 overflow-y-auto p-4">
+  {messages.map((msg, index) => (
+    <div
+      key={index}
+      className={`flex mb-3 ${
+        msg.role === "user"
+          ? "justify-end"
+          : "justify-start"
+      }`}
+    >
+      <div
+        className={`max-w-[70%] px-4 py-2 rounded-2xl ${
+          msg.role === "user"
+            ? "bg-blue-500 text-white"
+            : "bg-white"
+        }`}
+      >
+        {msg.text}
+      </div>
+    </div>
+  ))}
+</div>
+
+      <div className="p-4">
+        <div className="relative max-w-4xl mx-auto">
+              <i className="fa-solid fa-plus absolute left-4 top-1/2 -translate-y-1/2 text-xl cursor-pointer"></i>
+ <textarea
+  rows={1}
+  placeholder="Type a message..."
+ value={data}
+ onChange={handleChange}
+  className="
+    w-full
+    border border-slate-900
+    rounded-2xl
+    pl-12 pr-12 py-4
+    text-lg
+    resize-none
+    overflow-hidden
+    min-h-15
+    max-h-50
+  "
+/>
+          <i className="fa-solid fa-magnifying-glass-arrow-right absolute right-4 top-1/2 -translate-y-1/2 text-2xl cursor-pointer" onClick={handlleCLick}></i>
+        </div>
+      </div>
+
     </div>
   )
 }
